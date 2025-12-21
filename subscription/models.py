ecd3c131b87b99bbc965ceba07a8a17d3f8da579
John-SSL <jsscadden@gmail.com>
2025-12-21 12:23:24 +0000

    Create subscription app with plan and user subscription models

---
+++ subscription/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+# Create your models here.
+
+class Plan(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    max_backtests_per_day = models.IntegerField(null=True, blank=True)
+
+
+class UserSubscription(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    plan = models.ForeignKey(Plan, on_delete=models.SET_NULL, null=True)
+    stripe_customer_id = models.CharField(max_length=255, blank=True, null=True)
+    stripe_subscription_id = models.CharField(max_length=255, blank=True, null=True)
+    active_until = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def refresh_status(self):
+        """
+        Auto-update is_active based on active_until.
+        """
+        current_time = timezone.now()
+        self.is_active = (
+        self.active_until is not None and self.active_until > current_time)
+        self.save(update_fields=["is_active"])
