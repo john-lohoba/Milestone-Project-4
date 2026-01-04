ba099efb9179c3c40881732230ca7058cb75864c
John-SSL <jsscadden@gmail.com>
2026-01-04 21:52:25 +0000

    Refactor for deployment

--- tbo/settings.py
+++ tbo/settings.py
@@ -150,7 +150,15 @@ WSGI_APPLICATION = "tbo.wsgi.application"
 #         }
 #     }
 
-DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+# DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+DATABASES = {
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
+
+
 
 
 if "test" in sys.argv:
