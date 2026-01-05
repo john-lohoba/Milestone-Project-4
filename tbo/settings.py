7936a5c50d6f0ec20b214f05b32d8b7c27f73eea
John-SSL <jsscadden@gmail.com>
2026-01-05 15:52:56 +0000

    Update database

--- tbo/settings.py
+++ tbo/settings.py
@@ -138,17 +138,11 @@ WSGI_APPLICATION = "tbo.wsgi.application"
 #     }
 # }
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else: 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+
 
 
 
