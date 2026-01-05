c31d613ebc9bebf80eb73e1c8276e20c94362f0f
John-SSL <jsscadden@gmail.com>
2026-01-05 17:14:03 +0000

    Add favicons

--- tbo/settings.py
+++ tbo/settings.py
@@ -139,12 +139,17 @@ WSGI_APPLICATION = "tbo.wsgi.application"
 # }
 
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
-
-
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 if "test" in sys.argv:
